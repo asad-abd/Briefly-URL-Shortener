954e95e406d5dfc0624a20a271b6eb9cca281a9b
divyanshk443 <55971596+divyanshk443@users.noreply.github.com>
2022-02-09 01:43:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,21 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const { getLongUrl, addOrUpdateUrl, deleteUrl } = require("./dynamo");
+const rateLimit = require('express-rate-limit')
+const requestIp = require('request-ip');
 
 app.use(express.json());
 app.use(cors());
+app.use(requestIp.mw());
+
+app.use(rateLimit({
+  windowMs: 60 * 10*1000, // 1 minute
+  max: 4, // limit each IP to 30 requests per windowMs
+  message: "Your limit exceeded",
+  keyGenerator: (req, res) => {
+    return req.clientIp // IP address from requestIp.mw(), as opposed to req.ip
+  }
+}));
 
 // shorturl creation endpoint
 require("./routes/createShortUrl")(app);
